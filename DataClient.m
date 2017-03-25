4c7ce2e360fadda56c4b089447ed04dca9c8722b
ldk16 <ldk14@mails.tsinghua.edu.cn>
2017-03-26 01:46:39 +0800

    Data Client
    Receive data and process it.

---
+++ DataClient.m
@@ -0,0 +1,44 @@
+classdef DataClient<handle
+    %DATACLIENT TCP/IP data client
+    %   Detailed explanation goes here   
+ 
+    properties
+        TCPIP;
+        serverPort;
+        ipAddress;
+        sampleRate;
+        updateInterval;
+    end
+    
+    methods
+        function obj=DataClient(ipAddress, serverPort, sampleRate, bufferSize,updateInterval)
+            obj.updateInterval=updateInterval;
+            obj.serverPort = serverPort;
+            obj.ipAddress = ipAddress;
+            obj.sampleRate = sampleRate;
+            obj.TCPIP = tcpip(obj.ipAddress, obj.serverPort,'NetworkRole','Client');
+            if round(obj.sampleRate * obj.updateInterval) > 1
+                updatePoints = round(obj.sampleRate * obj.updateInterval);
+            else
+                updatePoints = obj.sampleRate;
+            end
+            obj.TCPIP.BytesAvailableFcnCount = 4 * updatePoints;
+            obj.TCPIP.BytesAvailableFcnMode = 'byte';
+            obj.TCPIP.BytesAvailableFcn = {@GET_DATA, bufferSize*obj.sampleRate};
+            obj.TCPIP.InputBufferSize = obj.TCPIP.BytesAvailableFcnCount * 10;
+            obj.TCPIP.ByteOrder = 'littleEndian';
+        end
+        
+        function Open(obj)
+            fopen(obj.TCPIP);
+        end
+        
+        function Close(obj)
+            fclose(obj.TCPIP);
+        end
+        
+        function [data] = GetBufferData(obj)
+            data = obj.TCPIP.UserData;
+        end
+    end
+end
