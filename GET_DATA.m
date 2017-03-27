59e1bd10eb7dffd0790f870c769387e3a68a0d75
ldk16 <ldk14@mails.tsinghua.edu.cn>
2017-03-27 19:54:07 +0800

    Update GET_DATA.m

--- GET_DATA.m
+++ GET_DATA.m
@@ -1,7 +1,9 @@
-function GET_DATA( obj, event, bufferSize )
+function GET_DATA( obj, event, bufferSize, fileName)
 %GetChannelDataCallBack Summary of this function goes here
 %   Detailed explanation goes here
     data = fread(obj, obj.BytesAvailableFcnCount/4, 'int');
+    %record all the data and save in file
+    dlmwrite(fileName,data,'-append');
     obj.UserData = [obj.UserData;data];
     userDataLength = length(obj.UserData);
     if userDataLength > bufferSize
