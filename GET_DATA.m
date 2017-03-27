3f313d355ab2a535f095cab47b588c59c852e23b
ldk16 <ldk14@mails.tsinghua.edu.cn>
2017-03-27 10:41:39 +0800

    Update GET_DATA.m
    Change the userdata to column wise

--- GET_DATA.m
+++ GET_DATA.m
@@ -2,7 +2,7 @@ function GET_DATA( obj, event, bufferSize )
 %GetChannelDataCallBack Summary of this function goes here
 %   Detailed explanation goes here
     data = fread(obj, obj.BytesAvailableFcnCount/4, 'int');
-    obj.UserData = [obj.UserData data'];
+    obj.UserData = [obj.UserData;data];
     userDataLength = length(obj.UserData);
     if userDataLength > bufferSize
         obj.UserData = obj.UserData(:, (end-bufferSize+1):end);
