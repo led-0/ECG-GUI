c3296bf33a287db464ba4b4b1cdfafa2ede3e624
ldk16 <ldk14@mails.tsinghua.edu.cn>
2017-04-10 11:46:10 +0800

    Update main.m

--- main.m
+++ main.m
@@ -1,31 +1,24 @@
 close all; clear all; 
-%% Create data client object
+%% Basic procedure
+% Create data client object
+UserName = 'User_default';
 ipAddress = 'localhost'; % Data server IP address
 serverPort = 55000; % Data server port%MCU will also send data to this port
 sampleRate = 250; % Sampling rate
 updateInterval = 0.04;
-bufferSize = 2; % Data buffer size, in seconds
-dataClient = DataClient('User1',ipAddress, serverPort, sampleRate, bufferSize,updateInterval); % Create client object
+bufferSize = 5; % Data buffer size, in seconds
+%% 
+%connect 这个按钮不用了
+%start
+dataClient = DataClient(ipAddress, serverPort, sampleRate, bufferSize,updateInterval,UserName); % Create client object
 dataClient.Open;
-data = dataClient.GetBufferData; % Get data from buffer
-%% Process Data
-%This part can show the data dynamically
-%If needed, it can be changed to fit the GUI part
-figure;
-hPlot = plot(dataClient.GetBufferData');
-%link dataClient.GetBufferData' to the figure 
-dataSourceName = 'dataClient.GetBufferData';
-set(hPlot, 'YDataSource', dataSourceName);
-linkdata on;
-telapsed = 0;
-tic;
-while true
-    telapsed = telapsed + toc;
-    if telapsed > updateInterval
-        data = dataClient.GetBufferData;
-        refreshdata(hPlot);
-        telapsed = 0;
-        tic;
-    end
-    pause(0.01);
-end
+%stop:
+dataClient.Close;
+%get ecg signal
+dataClient.GetRawData;
+%get R wave 这个数组记录所有R峰，下标与raw data 对应
+dataClient.GetRwave;
+%get Heart Rate
+dataClient.GetHR;
+%HRV analysis
+dataClient.HRV;
