67311871093fd105fca03241229c8ab86f73fc49
ldk16 <ldk14@mails.tsinghua.edu.cn>
2017-04-10 11:44:21 +0800

    Update DataClient.m

--- DataClient.m
+++ DataClient.m
@@ -8,17 +8,19 @@ classdef DataClient<handle
         ipAddress;
         sampleRate;
         updateInterval;
-        fileName;
+        bufferSize;
+        UserName;
     end
     
     methods
-        function obj=DataClient(UserName, ipAddress, serverPort, sampleRate, bufferSize,updateInterval)
-            obj.fileName=UserName+'.txt';
+        function obj=DataClient(ipAddress, serverPort, sampleRate, bufferSize,updateInterval,username)
             obj.updateInterval=updateInterval;
             obj.serverPort = serverPort;
             obj.ipAddress = ipAddress;
             obj.sampleRate = sampleRate;
             obj.TCPIP = tcpip(obj.ipAddress, obj.serverPort,'NetworkRole','Client');
+            obj.UserName=username;
+            obj.bufferSize=bufferSize;
             if round(obj.sampleRate * obj.updateInterval) > 1
                 updatePoints = round(obj.sampleRate * obj.updateInterval);
             else
@@ -26,7 +28,7 @@ classdef DataClient<handle
             end
             obj.TCPIP.BytesAvailableFcnCount = 4 * updatePoints;
             obj.TCPIP.BytesAvailableFcnMode = 'byte';
-            obj.TCPIP.BytesAvailableFcn = {@GET_DATA, bufferSize*obj.sampleRate,fileName};
+            obj.TCPIP.BytesAvailableFcn = {@GET_DATA, round(bufferSize*obj.sampleRate),obj.UserName};
             obj.TCPIP.InputBufferSize = obj.TCPIP.BytesAvailableFcnCount * 10;
             obj.TCPIP.ByteOrder = 'littleEndian';
         end
@@ -39,8 +41,40 @@ classdef DataClient<handle
             fclose(obj.TCPIP);
         end
         
-        function [data] = GetBufferData(obj)
-            data = obj.TCPIP.UserData;
+        function [signal]=GetRawData(obj)
+            if length(obj.TCPIP.UserData)<=round(obj.bufferSize*obj.sampleRate)
+                signal = obj.TCPIP.UserData;
+            else
+                signal = obj.TCPIP.UserData(1:round(obj.bufferSize*obj.sampleRate));
+            end
+        end
+        
+        function [R_wave]=GetRwave(obj)
+            if length(obj.TCPIP.UserData)<=round(obj.bufferSize*obj.sampleRate)
+                R_wave = [];
+            else
+                R_wave = obj.TCPIP.UserData(round(obj.bufferSize*obj.sampleRate)+1:end);
+            end
+        end
+        
+        function Heart_Rate=GetHR(obj)
+            R_wave=GetRwave(obj);
+            if isempty(R_wave);
+                [~,R_wave,~]=pan_tompkin(GetRawData,obj.sampleRate,0);             
+            end
+            Heart_Rate=length(R_wave(:))/obj.bufferSize*60;
+        end
+        
+        function HRV(obj)
+            ecg=dlmread([obj.UserName,'.txt']);
+            [~,r_i_raw,~]=pan_tompkin(ecg,obj.sampleRate,0);
+            % Calculate Heart Rate
+            t=sort(r_i_raw);
+            t=(t-t(1))/fs;
+            y=diff(t);
+            t(end)=[];
+            dlmwrite([obj.UserName,'.ibi'],[t(:),y(:)]);
+            HRVAS
         end
     end
 end
