bef61ca97f6eec6711b10ace9da37c8a56ff70b0
ldk16 <ldk14@mails.tsinghua.edu.cn>
2017-04-10 11:44:51 +0800

    Update GET_DATA.m

--- GET_DATA.m
+++ GET_DATA.m
@@ -1,12 +1,24 @@
-function GET_DATA( obj, event, bufferSize, fileName)
+function GET_DATA( obj, event, bufferSize ,UserName )
 %GetChannelDataCallBack Summary of this function goes here
 %   Detailed explanation goes here
-    data = fread(obj, obj.BytesAvailableFcnCount/4, 'int');
-    %record all the data and save in file
-    dlmwrite(fileName,data,'-append');
-    obj.UserData = [obj.UserData;data];
-    userDataLength = length(obj.UserData);
-    if userDataLength > bufferSize
-        obj.UserData = obj.UserData(:, (end-bufferSize+1):end);
+data = fread(obj, obj.BytesAvailableFcnCount/4, 'float');
+% 50Hz bandstop
+b=[0.7294,0,2.1883,0,2.1883,0,0.7294];
+a=[1,0,2.3741,0,1.9294,0,0.5321];
+data=filtfilt(b,a,data(:));
+%record all the data and save in file
+dlmwrite(UserName+'.txt',data,'-append');
+userDataLength = length(obj.UserData);
+if userDataLength >=bufferSize
+    signal=[obj.UserData(1:bufferSize);data];
+    if length(signal)>bufferSize
+        signal=signal((end-bufferSize+1):end);
     end
+    %fs=200
+    [~,r_i_raw,~]=pan_tompkin(signal,200,0);
+    obj.UserData = [signal(:);r_i_raw(:)];
+else
+    obj.UserData = [obj.UserData; data];
+end
+
 end
