b873cebc1c63ea03df4cc045265aca7845390f54
ldk16 <ldk14@mails.tsinghua.edu.cn>
2017-04-12 09:01:30 +0800

    Update DataServer.py

--- DataServer.py
+++ DataServer.py
@@ -1,22 +1,28 @@
 import socket
 import threading
 import time
+import struct
 
 
-def tcplink(sock, addr, share_data):
+share_data=[]
+
+
+def tcplink(sock, addr):
+    global share_data
     print('Accept new connection from %s:%s...' % addr)
     lock = threading.Lock()
     while True:
         if addr[0] == '127.0.0.1':
             lock.acquire()
-            sock.send(bytes(share_data, 'UTF-8'))
+            sock.send(struct.pack('unsigned short',share_data))
+            share_data = []
             lock.release()
         else:
+            temp = sock.recv(1024)
             lock.acquire()
-            share_data = sock.recv(1024)
-            if share_data == 'exit' or not share_data:
-                break
+            share_data = struct.unpack('unsigned short',temp)
             lock.release()
+            print('%d\n',share_data)
         time.sleep(0.1)
     sock.close()
     print('Connection from %s:%s closed.' % addr)
@@ -24,20 +30,16 @@ def tcplink(sock, addr, share_data):
 
 def main():
     serverPort = 55000
-    sampleRate = 250
-    bufferSize = 3 * sampleRate  # in seconds
-    updateInterval = 0.04
-    share_data = '12345'
     dataserver = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     dataserver.bind(('0.0.0.0', serverPort))
     # waiting maximum three connections
-    dataserver.listen(3)
+    dataserver.listen(2)
     print('Waiting for connection...')
     while True:
         # 接受一个新连接:
         sock, addr = dataserver.accept()
         # 创建新线程来处理TCP连接:
-        t = threading.Thread(target=tcplink, args=(sock, addr, share_data))
+        t = threading.Thread(target=tcplink, args=(sock, addr))
         t.start()
 
 
